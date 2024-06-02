b84148b94fe9784c347a997d8a3c3dc810dba83b
Listwas <listwa.patrykkr@gmail.com>
2024-06-02 11:56:29 +0200

    main block: end of a day countdown added

--- src/countdown/mainBlock.jsx
+++ src/countdown/mainBlock.jsx
@@ -1,13 +1,43 @@
 import s from "./mainBlock.module.css";
-function MainBlock() {
+import React, { useState, useEffect } from "react";
+
+function formatTime(unit) {
+  return unit < 10 ? `0${unit}` : unit;
+}
+
+function TimeDisplay({ hours, minutes, seconds }) {
   return (
-    <>
-      <main className={s.mainCont}>
-        <div className={s.columnCont}>
-          <p>COUNTDOWN</p>
-        </div>
-      </main>
-    </>
+    <div className={s.columnCont}>
+      <p>day will end in</p>
+      <p>
+        {formatTime(hours)}:{formatTime(minutes)}:{formatTime(seconds)}{" "}
+      </p>
+    </div>
   );
 }
+
+function MainBlock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const dayNow = new Date();
+  const hours = 24 - dayNow.getHours();
+  const minutes = 59 - dayNow.getMinutes();
+  const seconds = 59 - dayNow.getSeconds();
+
+  return (
+    <main className={s.mainCont}>
+      <TimeDisplay hours={hours} minutes={minutes} seconds={seconds} />
+    </main>
+  );
+}
+
 export default MainBlock;
