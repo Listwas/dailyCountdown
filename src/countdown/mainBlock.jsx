75678299962ef323ba80cf4a5bab687457c3844f
Listwas <listwa.patrykkr@gmail.com>
2024-06-02 10:59:24 +0200

    added time component and displaying current time on the top

--- src/countdown/mainBlock.jsx
+++ src/countdown/mainBlock.jsx
@@ -1,12 +1,10 @@
 import s from "./mainBlock.module.css";
-
 function MainBlock() {
   return (
     <>
       <main className={s.mainCont}>
         <div className={s.columnCont}>
           <p>COUNTDOWN</p>
-          <p>time</p> {/* get time from os */}
         </div>
       </main>
     </>
--- src/navBar/navBar.jsx
+++ src/navBar/navBar.jsx
@@ -1,9 +1,15 @@
 import s from "./navBar.module.css";
+import CurrentDate from "../currentDate";
 
 function NavBar() {
   return (
     <nav className={s.mainCont}>
       <div className={s.navButton}>
+        <div style={{ width: "50px" }}></div>
+        <div className={s.time}>
+          <CurrentDate />
+        </div>
+
         <button></button>
       </div>
     </nav>
